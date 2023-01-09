7e1a00812bf31e30d6b297d7240cc775965c33ea
willow-rubenstein <contact.notashe@gmail.com>
2023-01-09 12:30:24 -0500

    added actual example for instagram hack from a real dm I got. TODO: take screenshots of process and outline red flags.

--- src/pages/subpages/discord/2.js
+++ src/pages/subpages/discord/2.js
@@ -1,6 +1,8 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useState } from "react";
 import { Item } from "../../../components/item";
+import Slideshow from "../../../components/slideshow";
 
 export default function Page2(props) {
     return (
@@ -11,7 +13,7 @@ export default function Page2(props) {
             flexWrap: "wrap", 
             alignContent: "center"
         }}>
-            <Card sx={{maxWidth: "500px"}}>
+            <Card sx={{width: "100%"}}>
                 <CardContent>
                     <Typography variant="h5" component="div">
                         What are QR Code Scams?
@@ -31,23 +33,19 @@ export default function Page2(props) {
                     </Typography>
                 </CardContent>
             </Card>
-            <Card sx={{maxWidth: "500px"}}>
+            <Card sx={{width: "100%"}}>
                 <CardContent>
                     <Typography variant="h5" component="div">
-                        What are QR Code Scams?
-                    </Typography>
-                    <Typography>
-                        You may have noticed, at one point or another, that
-                        when logging into Discord on a new computer, you are prompted
-                        for a QR Code to log in using your phone app. These scams in particular
-                        that utilize this feature to steal your account by sending
-                        your login credentials to a foreign source, which will be shown
-                        below using an example from GitHub of the scam.
+                        Example of a Real QR Login Prompt
                     </Typography>
                     <br/>
+                    <Slideshow images={[
+                        "assets/discord/loginpage_example.png",
+                        "assets/discord/mobile_login_prompt.jpg"
+                    ]} />
                     <Typography variant="body2" color="text.secondary">
-                        These scams can be avoided easily by just not scanning the QR code,
-                        and to be completely real, most Discord users don't even use the feature to my knowledge anyway.
+                        The images above are the real Discord 
+                        pages (desktop and mobile, set to my account) for QR Auth.
                     </Typography>
                 </CardContent>
             </Card>
---
+++ src/pages/subpages/twitter/2.js
@@ -0,0 +1,11 @@
+export default function Page2(props) {
+    return (
+        <div>
+            {
+                /* Instagram hack url (real)
+                http://l.instagram.com/?h31lo_wor1d=1496588401931345932&e=ATM_m55xEe_V8pJvU-noKPGeuhvGGBlnCItFkDhfKtkK4qHWIFXuFC9IrfUCCTxUjBy7ZCiE&s=1&u=http%3A%2F%2Fbusiness.instagram.com%2Fmicro_site%2Furl%2F%3Fevent_type%3Dclick%26site%3Digb%26destination%3Dhttps%253A%252F%252Fwww.facebook.com%252Fads%252Fig_redirect%252F%253Fd%253DAd-9UUpXVqVLNUx_LYaKZeKek5oYlWjoEdXwzKgPbUE9qRpf4p77ahkKJVJ0kuzPpFtsCo6iNW3tiZLDTY2LPR4xCa63d0ycYdTB4uq9n11GSU2h81N4csFuuiw8b0crQB08jWUYW08n1cc2LujG0j00JXW6R7_-_xEeAwWPs56HHv7PQhgj6ktkhSEU5AhwI8vAMSgikxz8VtJ4JiQRzIjL%2526a%253D1%2526hash%253DAd9_0TyBjc1n8sHe
+                */
+            }
+        </div>
+    )
+}
